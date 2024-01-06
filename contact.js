dd64c03f279ad6f6d8ee6a17e0ec12688fc5f2bb
Dave Shoreman <dshoreman@users.noreply.github.com>
2024-01-06 18:50:32 +0000

    Improve name handling
    `FN` might actually be friendly name or similar, but until I unlazy
    myself enough to read into the actual fields in the spec, this'll do.

--- contact.js
+++ contact.js
@@ -17,21 +17,28 @@ export default class Contact {
     }
 
     #extractFullName(line) {
+        this.fullName = line.replace('FN:', '');
+
+        if (this.fullName !== `${this.firstName} ${this.lastName}`.trim()) {
+            this.fullName += ` (${this.firstName} ${this.lastName})`;
+        }
+    }
+
+    #extractName(line) {
         const [_, last, first] = line.match(/N:(.*)?;(.*)?;;;/);
 
-        if (first && last) {
-            this.fullName = `${first} ${last}`;
-        } else if (first) {
-            this.fullName = first;
-        } else {
-            this.fullName = last;
-        }
+        this.firstName = first || '';
+        this.lastName = last || '';
+        this.nameRaw = line.split(':', 2)[1];
     }
 
     #parseLines() {
         for (const line of this.rawData.split(/[\r\n]+/)) {
             switch (true) {
                 case line.startsWith('N:'):
+                    this.#extractName(line);
+                    break;
+                case line.startsWith('FN:'):
                     this.#extractFullName(line);
                     break;
                 case /^(BEGIN|END):VCARD$/.test(line):
