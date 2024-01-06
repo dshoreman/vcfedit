25e691f968800e5737953db008bab6163df3b166
Dave Shoreman <dshoreman@users.noreply.github.com>
2024-01-06 19:55:26 +0000

    Parse some more fields

--- contact.js
+++ contact.js
@@ -10,10 +10,17 @@ export default class Contact {
     vCard() {
         const clone = this.template.cloneNode(true);
 
-        clone.querySelector('li').innerText = this.fullName;
+        clone.querySelector('b').innerText = this.fullName;
+        clone.querySelector('em').innerText = this.phone || '';
+        clone.querySelector('span').innerText = this.email || '';
+        clone.querySelector('sup').innerText = this.organisation || '';
         clone.toString = () => this.rawData;
 
         return clone;
+    }
+
+    #extractEmail(line) {
+        this.email = line.replace('EMAIL:', '');
     }
 
     #extractFullName(line) {
@@ -29,7 +36,15 @@ export default class Contact {
 
         this.firstName = first || '';
         this.lastName = last || '';
-        this.nameRaw = line.split(':', 2)[1];
+        this.nameRaw = line.substring(2);
+    }
+
+    #extractOrganisation(line) {
+        this.organisation = line.substring(4);
+    }
+
+    #extractPhone(line) {
+        this.phone = line.replace('TEL;CELL:', '');
     }
 
     #parseLines() {
@@ -41,7 +56,17 @@ export default class Contact {
                 case line.startsWith('FN:'):
                     this.#extractFullName(line);
                     break;
+                case line.startsWith('TEL;CELL:'):
+                    this.#extractPhone(line);
+                    break;
+                case line.startsWith('EMAIL:'):
+                    this.#extractEmail(line);
+                    break;
+                case line.startsWith('ORG:'):
+                    this.#extractOrganisation(line);
+                    break;
                 case /^(BEGIN|END):VCARD$/.test(line):
+                case /VERSION:[0-9\.]+/.test(line):
                     break; // no-op
                 default: console.warn("Unhandled VCF line: " + line);
             }
